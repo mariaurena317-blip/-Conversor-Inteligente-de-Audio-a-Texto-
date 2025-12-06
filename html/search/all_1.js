14109029679fb1dd4cdbcedb45565e0ddc10c087
mariaurena317-blip <mariaurena317@gmail.com>
2025-12-06 01:23:24 -0400

    Add files via upload

---
+++ html/search/all_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['con_20criptografia_0',['Conversor de Audio a Texto con Criptografia',['../index.html',1,'']]],
+  ['conversor_1',['Conversor',['../namespace_conversor.html',1,'']]],
+  ['conversor_20de_20audio_20a_20texto_20con_20criptografia_2',['Conversor de Audio a Texto con Criptografia',['../index.html',1,'']]],
+  ['criptografia_3',['Conversor de Audio a Texto con Criptografia',['../index.html',1,'']]]
+];
---
+++ html/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "acdefijoptu",
+  1: "f",
+  2: "c",
+  3: "fp",
+  4: "df",
+  5: "acdefijoptu"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Clases",
+  2: "Espacios de nombres",
+  3: "Archivos",
+  4: "Funciones",
+  5: "Páginas"
+};
+
