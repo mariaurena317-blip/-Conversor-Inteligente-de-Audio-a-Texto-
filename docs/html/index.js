f6aa824a0f7da35074fab509dd13b328f3eda0c0
mariaurena317-blip <mariaurena317@gmail.com>
2025-12-06 01:29:00 -0400

    Delete docs directory

--- docs/html/index.js
+++
@@ -1,9 +0,0 @@
-var index =
-[
-    [ "Introducción", "index.html#intro", null ],
-    [ "Justificacion del Proyecto", "index.html#justificacion", null ],
-    [ "Objetivos del Proyecto", "index.html#objetivos", null ],
-    [ "Funcionalidades Principales", "index.html#funciones", null ],
-    [ "Tecnologías Utilizadas", "index.html#tecnologias", null ],
-    [ "Estructura del Proyecto", "index.html#estructura", null ]
-];
